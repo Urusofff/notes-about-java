e73656bd628aeaaa2857949bee4263870ea402c4
urusoff <mks.urusov@gmail.com>
2023-08-17 18:15:48 +0300

    updated version with idea files

--- src/main/java/vk/com/notes_about_java/client/Client.java
+++ src/main/java/vk/com/notes_about_java/client/Client.java
@@ -1,6 +1,5 @@
 package vk.com.notes_about_java.client;
 
-import lombok.Getter;
 
 public record Client(Integer id, Integer groupId, String name) {
 
--- src/main/java/vk/com/notes_about_java/client/ClientGroup.java
+++ src/main/java/vk/com/notes_about_java/client/ClientGroup.java
@@ -1,7 +1,5 @@
 package vk.com.notes_about_java.client;
 
-import lombok.Getter;
-
 import java.util.Set;
 
 public record ClientGroup(Integer id, String name, Set<Client> clients) {
